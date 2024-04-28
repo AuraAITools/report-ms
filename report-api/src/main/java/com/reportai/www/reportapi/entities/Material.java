7941d9c9c8bdf0be445bf02610b81373ee9b7b28
Kevin Liu Kai <kevinliusingapore@gmail.com>
2024-04-28 22:56:50 +0800

    feat: added materials entity

--- report-api/src/main/java/com/reportai/www/reportapi/entities/Material.java
+++ report-api/src/main/java/com/reportai/www/reportapi/entities/Material.java
@@ -1,24 +1,21 @@
 package com.reportai.www.reportapi.entities;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
-import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -28,19 +25,16 @@ import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "Institutions")
-@Setter
-@Getter
+@Table(name = "materials")
 @Builder
-@AllArgsConstructor
+@Getter
+@Setter
 @NoArgsConstructor
-public class Institution {
-
+@AllArgsConstructor
+public class Material {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID Id;
-
-    private String name;
 
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
@@ -48,37 +42,17 @@ public class Institution {
     @Column(name = "modified_at")
     private LocalDateTime modifiedAt;
 
+    private String name;
+
+    private String fileUrl;
+
+    @ManyToMany
+    @JsonIgnore
+    private List<Lesson> lessons;
+
     @OneToOne
-    @JsonBackReference
-    private User user;
-
-    @ManyToMany
     @JsonIgnore
-    private List<Student> students;
-
-    @ManyToMany
-    @JsonIgnore
-    private List<Parent> parents;
-
-    @ManyToMany
-    @JsonIgnore
-    private List<Educator> educators;
-
-    @OneToMany(cascade = CascadeType.ALL)
-    @JsonIgnore
-    private List<Class> classes;
-
-    @OneToMany
-    @JsonIgnore
-    private List<Invoice> invoices;
-
-    @OneToMany
-    @JsonIgnore
-    private List<TestGroup> testGroups;
-
-    @OneToMany
-    @JsonIgnore
-    private List<Material> materials;
+    private Institution institution;
 
     @PrePersist
     private void onCreate() {
