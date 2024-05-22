6e2dd3b4ee8563cf8b9e956115d949c3f4a3c518
Kevin Liu Kai <kevinliusingapore@gmail.com>
2024-05-22 21:47:48 +0800

    Revert "feat: implement oauth2resourceServer authentication using symmetric key"
    This reverts commit 602b4e1303b6585090b060fbfd2cf36b55af6643.

--- report-api/src/main/java/com/reportai/www/reportapi/ReportApiApplication.java
+++ report-api/src/main/java/com/reportai/www/reportapi/ReportApiApplication.java
@@ -1,12 +1,9 @@
 package com.reportai.www.reportapi;
 
-import com.reportai.www.reportapi.configproperties.OauthResourceServerConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({OauthResourceServerConfigProperties.class})
 public class ReportApiApplication {
 
 	public static void main(String[] args) {
