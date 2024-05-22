6a89e63d110abd3cae4b245c67c3bbb0dd976042
Kevin Liu Kai <kevinliusingapore@gmail.com>
2024-05-22 23:07:21 +0800

    feat: disabled CSRF protection and enable stateless session management

--- report-api/src/main/java/com/reportai/www/reportapi/config/OAuth2ResourceServerSecurityConfiguration.java
+++ report-api/src/main/java/com/reportai/www/reportapi/config/OAuth2ResourceServerSecurityConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
@@ -31,6 +33,8 @@ public class OAuth2ResourceServerSecurityConfiguration {
     @Bean
     SecurityFilterChain apiSecurity(HttpSecurity http) throws Exception {
         http
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((auth)-> auth
                         .anyRequest().authenticated()
                 )
--- report-api/src/main/java/com/reportai/www/reportapi/configproperties/OauthResourceServerConfigProperties.java
+++ report-api/src/main/java/com/reportai/www/reportapi/configproperties/OauthResourceServerConfigProperties.java
@@ -2,19 +2,18 @@ package com.reportai.www.reportapi.configproperties;
 
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-
-import java.util.Objects;
-
-import static java.util.Objects.isNull;
 
 @Slf4j
 @Component
 @ConfigurationProperties(prefix = "oauth2.jwt")
 @Getter
+@Setter
 @ConditionalOnProperty(name = "oauth2.security.enabled", havingValue = "true")
 public class OauthResourceServerConfigProperties {
 
