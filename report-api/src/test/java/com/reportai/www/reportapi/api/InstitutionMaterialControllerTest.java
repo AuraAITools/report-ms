f10234ae46d80e923dd09a15dac3464d4c738d49
LeeMingDe <58969901+LeeMingDe@users.noreply.github.com>
2024-05-27 00:01:21 +0800

    fix: add profile annotation

--- report-api/src/test/java/com/reportai/www/reportapi/api/InstitutionMaterialControllerTest.java
+++ report-api/src/test/java/com/reportai/www/reportapi/api/InstitutionMaterialControllerTest.java
@@ -30,6 +30,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -37,6 +38,7 @@ import org.springframework.test.web.servlet.MvcResult;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class InstitutionMaterialControllerTest {
 
     @Autowired
