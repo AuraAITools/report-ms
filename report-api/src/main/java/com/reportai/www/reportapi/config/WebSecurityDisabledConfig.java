79d0a8756c7bfa07b945b7647834b437c7937d08
kevin-otter-liu <kevinliusingapore@gmail.com>
2024-10-27 23:32:40 +0800

    fix(h2): this fixes h2 console not showing

--- report-api/src/main/java/com/reportai/www/reportapi/config/WebSecurityDisabledConfig.java
+++ report-api/src/main/java/com/reportai/www/reportapi/config/WebSecurityDisabledConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -25,6 +26,7 @@ public class WebSecurityDisabledConfig {
         http
                 .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
+                .headers(HeadersConfigurer::disable)
                 .authorizeHttpRequests((auth) -> auth.anyRequest().permitAll());
         return http.build();
     }
